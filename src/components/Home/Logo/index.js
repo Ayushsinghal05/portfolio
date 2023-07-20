f37d61974399234cf56a0c9df162d08dbdcbb881
Ayushsinghal05 <ayushisawesome7@gmail.com>
2023-07-19 19:07:52 -0700

    remove gsap

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,45 +1,14 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/my_pic.jpg'
 import './index.scss'
 
 const Logo = () => {
-  const bgRef = useRef()
-  const solidLogoRef = useRef()
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
-
   return (
-    <div className="logo-container" ref={bgRef}>
+    <div className="logo-container">
       <img
         className="solid-logo"
-        ref={solidLogoRef}
         src={LogoS}
         alt="My pic"
       />
-
     </div>
   )
 }
