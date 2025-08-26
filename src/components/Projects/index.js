9f302f307b293167c98e96560c1fbfcc5dfc8832
AyushSinghal05 <singhal.ayush230@gmail.com>
2025-08-25 20:21:38 -0400

    Added Shell project to Projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 import intellijIcon from "../../assets/images/IntelliJ_IDEA_Icon.svg.png";
+import cppIcon from "../../assets/images/cplusplus.svg";
 
 const Projects = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -22,6 +23,30 @@ const Projects = () => {
             </h1>
             
             <div className="images-container">
+                <div className="image-box">
+                    <div className="projects-image">
+                        <img 
+                            src={cppIcon}
+                            alt="Shell Interpreter"
+                        />
+                        <button 
+                            className="btn" 
+                            onClick={() => window.open('https://github.com/Ayushsinghal05/Shell', '_blank')}
+                        >
+                            View Project
+                        </button>
+                    </div>
+                    <div className="content">
+                        <p className="title">Shell Interpreter</p>
+                        <h4 className="description">C++ System Programming</h4>
+                        <p className="description">
+                            A custom Unix shell implementation written in C++ with advanced features including 
+                            command parsing, I/O redirection, pipes, background processes, wildcard expansion, 
+                            and built-in commands. Features lexical analysis with Flex and syntax parsing with Yacc/Bison.
+                        </p>
+                    </div>
+                </div>
+                
                 <div className="image-box">
                     <div className="projects-image">
                         <img 
