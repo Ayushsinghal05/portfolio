0a349a9c24940b04588c1dd523c81368386bdb10
AyushSinghal05 <singhal.ayush230@gmail.com>
2025-08-26 10:55:49 -0400

    Fixed routing issue

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-    ,
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    </HashRouter>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
